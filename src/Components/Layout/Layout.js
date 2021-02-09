271b189114f04ec9995f66ff1395ec51a561ef16
LusineAm <usine.a.amiraghyan@gmail.com>
2021-02-09 15:59:17 +0400

    created layout

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import Footer from '../Footer/Footer';
+
 import './Layout.scss';
 
 const Layout = () => {
     return(
-        <>
+        <div>
+            <main className="app-layout-content">
+            </main>
             <Footer />
-        </>
+        </div>
     )
 }
 
---
+++ src/Containers/Links/Link.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Links.scss';
+
+const Links = () => {
+    return(
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Links;
