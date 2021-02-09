1fffe15cf34b48c814ee1808a6423b3510abc247
LusineAm <usine.a.amiraghyan@gmail.com>
2021-02-09 11:41:30 +0400

    created landing page

---
+++ src/Containers/Header/Headrer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header = () => {
+    return(
+        <div className="app-header">
+            
+        </div>
+    )
+}
+
+export default Header;
